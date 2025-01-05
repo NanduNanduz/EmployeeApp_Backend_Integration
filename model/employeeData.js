5f31be4e88a8c438f2a548e2f03a17010a437290
NanduNanduz <nandunsb99@gmail.com>
2025-01-06 01:14:31 +0530

    created schema for employees

---
+++ model/employeeData.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');//import mongoose
+
+//Create Schema
+const employeeSchema = mongoose.Schema({
+    employeeName : String,
+    employeeDesignation :String,
+    employeeLocation : String
+})
+
+const employeeData = mongoose.model('employee', employeeSchema);//mapping schema to the collection
+
+module.exports = employeeData;//export particular schema
