0268d0a3e749afc7e94122178ecc524a06676ea4
NanduNanduz <nandunsb99@gmail.com>
2025-01-08 00:29:49 +0530

    updated routes with post , put , and delete operations

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -23,14 +23,46 @@ function employeeroutes(nav){
         }
     })
 
+    //post
+    router.post('/addEmployee',async(req,res)=>{
+        try {
+            const item = new employeeModel(req.body); //Create a new document using the req.body
+            const savedItem = await item.save();// save the doc to the database
+            res.redirect('/employees');          
+        } catch (error) {
+            res.status(404).send("Post Unsuccessful")
+            
+        }
+    })
+
+    //update
+    router.post('/editEmployeee/:id',async(req,res)=>{
+        try {
+            const editData = await employeeModel.findByIdAndUpdate(req.params.id, req.body);
+            res.redirect('/employees')
+            
+        } catch (error) {
+            res.status(404).send("Update UnSuccessful");
+            
+        }
+    })
+
+
+    //delete
+    router.delete('/delete/:id', async(req,res)=>{
+        try {
+            const deleteData = await employeeModel.findByIdAndDelete(req.params.id);
+            if(deleteData){
+                res.status(200).send('Delete Successful')
+            }
+        } catch (error) {
+            res.status(404).send('Delete Unsuccessful')
+            
+        }
+    })
 
 return router;
 }
-
-
-
-
-
 
 
 
